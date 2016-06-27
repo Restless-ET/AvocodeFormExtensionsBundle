dcabd5c728c2af1e54ef0bbb7268dfce60d3b399
Restless-ET <restless86@gmail.com>
2016-06-27 11:22:45 +0100

    Replaced deprecated 'Twig_Function_Method' and 'Twig_Filter_Method' with the new 'Twig_SimpleFunction' and 'Twig_SimpleFilter' methods on twig extensions. Bumped minimum twig version to 1.12.0.

--- Twig/Extension/ImageAssetsExtension.php
+++ Twig/Extension/ImageAssetsExtension.php
@@ -6,15 +6,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Container;
 
 /**
- * This extension adds common twig function for various upload manager 
- * bundles and common twig filter image manipulation bundles. 
- * 
- * Depending on %avocode.form.upload_manager% setting a diffrent 
+ * This extension adds common twig function for various upload manager
+ * bundles and common twig filter image manipulation bundles.
+ *
+ * Depending on %avocode.form.upload_manager% setting a diffrent
  * upload manager bundle is used.
- * 
- * Depending on %avocode.form.image_manipulator% setting a diffrent 
+ *
+ * Depending on %avocode.form.image_manipulator% setting a diffrent
  * image manipulation bundle is used.
- * 
+ *
  * @author Piotr Gołębiewski <loostro@gmail.com>
  */
 class ImageAssetsExtension extends \Twig_Extension
@@ -32,7 +32,7 @@ class ImageAssetsExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'image_asset'   =>  new \Twig_Function_Method($this, 'asset'),
+            new \Twig_SimpleFunction('image_asset', array($this, 'asset')),
         );
     }
 
@@ -42,10 +42,10 @@ class ImageAssetsExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'image_filter'  =>  new \Twig_Filter_Method($this, 'filter'),
+            new \Twig_SimpleFilter('image_filter', array($this, 'filter')),
         );
     }
-    
+
     /**
      * Gets the browser path for the image and filter to apply.
      *
@@ -54,22 +54,22 @@ class ImageAssetsExtension extends \Twig_Extension
     public function asset($object, $field)
     {
         $params = func_get_args();
-        
+
         if ('vich_uploader' === $this->getUploadManager()) {
             $ext = new \Vich\UploaderBundle\Twig\Extension\UploaderExtension(
-                $this->container->get('vich_uploader.templating.helper.uploader_helper')  
+                $this->container->get('vich_uploader.templating.helper.uploader_helper')
             );
-                        
+
             // Overwrite the fieldname with the needed mappingname by Vich
             $params[1] = $this->container->get('vich_uploader.property_mapping_factory')->fromField($object, $field)->getMappingName();
-            
+
             return call_user_func_array(array($ext, "asset"), $params);
         }
-        
+
         // In case no upload manager is used we expect object to have
         // a special method returning file's path
         $getter = "get".Container::Camelize($field)."WebPath";
-            
+
         return $object->$getter();
     }
 
@@ -82,23 +82,23 @@ class ImageAssetsExtension extends \Twig_Extension
     {
         $params = func_get_args();
         $path = $params[0];
-        
+
         if ('liip_imagine' === $this->getImageManipulator()) {
             $ext = new \Liip\ImagineBundle\Templating\ImagineExtension(
                 $this->container->get('liip_imagine.cache.manager')
             );
-            
+
             return call_user_func_array(array($ext, "filter"), $params);
         }
-        
+
         if ('avalanche_imagine' === $this->getImageManipulator()) {
             $ext = new \Avalanche\Bundle\ImagineBundle\Templating\ImagineExtension(
                 $this->container->get('imagine.cache.path.resolver')
             );
-            
+
             return call_user_func_array(array($ext, "applyFilter"), $params);
         }
-        
+
         // In case no image manipulator is used we
         // return the unmodified path
         return $path;
